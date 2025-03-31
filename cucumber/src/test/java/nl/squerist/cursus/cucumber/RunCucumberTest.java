cab36684cf95ab4cb1a83266f1aae73c87e90477
rvrijhoef <ruud.vrijhoef@gmail.com>
2025-03-31 10:05:21 +0200

    Add cucumberoptions to run all features

--- cucumber/src/test/java/nl/squerist/cursus/cucumber/RunCucumberTest.java
+++ cucumber/src/test/java/nl/squerist/cursus/cucumber/RunCucumberTest.java
@@ -5,7 +5,12 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(
+        features = {"src/test/resources/nl/squerist/cursus/cucumber/features/"},
+        glue = {"StepDefs"},
+        plugin = {"pretty"},
+        publish = true
+)
 public class RunCucumberTest {
 
 }
